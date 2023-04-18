c988b3e771191b2f05c8352b314f9edca2a2f07d
luanmacea <luanmacea@gmail.com>
2023-04-18 20:20:21 -0300

    Aprendendo a fazer map para encontrar texto

--- rm98290/js/script.js
+++ rm98290/js/script.js
@@ -14,3 +14,23 @@ function SeuPai() {
     let SeuPai = "Seu Pai e Rosa"
     console.log(SeuPai)
 }
+
+
+//convertendo node list em array spread
+const arrayelements = [...document.querySelectorAll("li")];
+// arrayelements.map((el,key)=>{
+    
+//     if (el.textContent == "Item-24") {
+//         el.textContent = `${key+1}[ Item-24 ]`;
+//     }
+
+// })
+
+
+// arrayelements.map((el,key)=>{
+    
+//     if (el.textContent.indexOf("1") != -1) {
+//         el.textContent = `[ ${el.textContent}]`;
+//     }
+
+// })
