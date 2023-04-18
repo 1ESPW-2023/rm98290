483335e2b247cdb6166bbe996b0c62f2b3354ff2
luanmacea <luanmacea@gmail.com>
2023-04-18 20:58:17 -0300

    Inicializacao css, mudando as cores em jss

--- rm98290/js/script.js
+++ rm98290/js/script.js
@@ -1,23 +1,23 @@
-const divelement = document.getElementById("div-box")
+// const divelement = document.getElementById("div-box")
 
 // divelement.addEventListener("click", ()=>{
 //     console.error(divelement);
 // })
 
-divelement.addEventListener("click", SeuPai);
+// divelement.addEventListener("click", SeuPai);
 
-function mostraelementos() {
-    console.log(divelement)
-}
+// function mostraelementos() {
+//     console.log(divelement)
+// }
 
-function SeuPai() {
-    let SeuPai = "Seu Pai e Rosa"
-    console.log(SeuPai)
-}
+// function SeuPai() {
+//     let SeuPai = "Seu Pai e Rosa"
+//     console.log(SeuPai)
+// }
 
 
-//convertendo node list em array spread
-const arrayelements = [...document.querySelectorAll("li")];
+// convertendo node list em array spread
+// const arrayelements = [...document.querySelectorAll("li")];
 // arrayelements.map((el,key)=>{
     
 //     if (el.textContent == "Item-24") {
@@ -34,3 +34,23 @@ const arrayelements = [...document.querySelectorAll("li")];
 //     }
 
 // })
+
+let tmp = 0
+
+function mudacor() {
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+
+    const elemento = [...document.getElementsByClassName("conteudo")];
+    elemento.forEach( (el)=>{
+        el.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } )
+    tmp = setTimeout(mudacor, 5000)
+}
+
+function pararTimeOut() {
+    clearTimeout(tmp);
+}
+
+mudacor()
